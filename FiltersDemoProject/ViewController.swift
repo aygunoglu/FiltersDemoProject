b3237c6e0cd55fdd1a5bb80ef61c5250e1a71a55
aygunoglu <hasanaygunoglu@gmail.com>
2021-08-05 17:30:00 +0300

    fixed the bloom filter. implemented  optional chaining to where required in order to make filter functions safer.

--- FiltersDemoProject/ViewController.swift
+++ FiltersDemoProject/ViewController.swift
@@ -21,73 +21,87 @@ class ViewController: UIViewController {
 
 
 
-    private func blurFilter(_ img: UIImage, intensity: Double) -> UIImage {
-        
-        let currentFilter = CIFilter(name: "CIGaussianBlur")
-        let beginImage = CIImage(image: img)
-        currentFilter!.setValue(beginImage, forKey: kCIInputImageKey)
-        currentFilter!.setValue(intensity, forKey: kCIInputRadiusKey)
-
-        let cropFilter = CIFilter(name: "CICrop")
-        cropFilter!.setValue(currentFilter!.outputImage, forKey: kCIInputImageKey)
-        cropFilter!.setValue(CIVector(cgRect: beginImage!.extent), forKey: "inputRectangle")
-
-        let output = cropFilter!.outputImage
-        let cgimg = context.createCGImage(output!, from: output!.extent)
-        let processedImage = UIImage(cgImage: cgimg!)
-        return processedImage
+    private func blurFilter(_ img: UIImage, intensity: Double) -> UIImage? {
+        if let startImage = CIImage(image: img) {
+            
+            if let filter = CIFilter(name: "CIGaussianBlur") {
+                filter.setValue(startImage, forKey: kCIInputImageKey)
+                filter.setValue(intensity, forKey: kCIInputRadiusKey)
+                
+                if let cropFilter = CIFilter(name: "CICrop") {
+                    cropFilter.setValue(filter.outputImage, forKey: kCIInputImageKey)
+                    cropFilter.setValue(CIVector(cgRect: startImage.extent), forKey: "inputRectangle")
+                    let output = cropFilter.outputImage
+                    let cgimg = context.createCGImage(output!, from: output!.extent)
+                    let newImage = UIImage(cgImage: cgimg!)
+                    return newImage
+                }
+            }
+            
+        }
+        return UIImage()
     }
     
-    private func invertFilter(_ img: UIImage) -> UIImage {
-        let startImage = CIImage(image: img)
+    private func invertFilter(_ img: UIImage) -> UIImage? {
+        if let startImage = CIImage(image: img) {
 
-        if let filter = CIFilter(name: "CIColorInvert") {
-            filter.setValue(startImage, forKey: kCIInputImageKey)
-            let cgImage = context.createCGImage(filter.outputImage!, from: filter.outputImage!.extent)
-            let newImage = UIImage(cgImage: cgImage!)
-            return newImage
+            if let filter = CIFilter(name: "CIColorInvert") {
+                filter.setValue(startImage, forKey: kCIInputImageKey)
+                let cgImage = context.createCGImage(filter.outputImage!, from: filter.outputImage!.extent)
+                let newImage = UIImage(cgImage: cgImage!)
+                return newImage
+            }
         }
-        return img
+        return UIImage()
     }
     
     private func sepiaFilter(_ img: UIImage, intensity: Double) -> UIImage {
-        let startImage = CIImage(image: img)
+        if let startImage = CIImage(image: img) {
 
-        if let filter = CIFilter(name: "CISepiaTone") {
-            filter.setValue(startImage, forKey: kCIInputImageKey)
-            filter.setValue(intensity, forKey: kCIInputIntensityKey)
-            let cgImage = context.createCGImage(filter.outputImage!, from: filter.outputImage!.extent)
-            let newImage = UIImage(cgImage: cgImage!)
-            return newImage
+            if let filter = CIFilter(name: "CISepiaTone") {
+                filter.setValue(startImage, forKey: kCIInputImageKey)
+                filter.setValue(intensity, forKey: kCIInputIntensityKey)
+                let cgImage = context.createCGImage(filter.outputImage!, from: filter.outputImage!.extent)
+                let newImage = UIImage(cgImage: cgImage!)
+                return newImage
+            }
         }
-        return img
+        return UIImage()
     }
     
     private func bloomFilter(_ img: UIImage, intensity: Double, radius: Double) -> UIImage {
-        let startImage = CIImage(image: img)
+        if let startImage = CIImage(image: img) {
 
-        if let filter = CIFilter(name: "CIBloom") {
-            filter.setValue(startImage, forKey: kCIInputImageKey)
-            filter.setValue(intensity, forKey: kCIInputIntensityKey)
-            filter.setValue(radius, forKey: kCIInputRadiusKey)
-            //let context = CIContext(options: nil)
-            let cgImage = context.createCGImage(filter.outputImage!, from: filter.outputImage!.extent)
-            let newImage = UIImage(cgImage: cgImage!)
-            return newImage
+            if let filter = CIFilter(name: "CIBloom") {
+                filter.setValue(startImage, forKey: kCIInputImageKey)
+                filter.setValue(intensity, forKey: kCIInputIntensityKey)
+                filter.setValue(radius, forKey: kCIInputRadiusKey)
+                
+                if let cropFilter = CIFilter(name: "CICrop") {
+                    cropFilter.setValue(filter.outputImage, forKey: kCIInputImageKey)
+                    cropFilter.setValue(CIVector(cgRect: startImage.extent), forKey: "inputRectangle")
+                    let output = cropFilter.outputImage
+                    let cgimg = context.createCGImage(output!, from: output!.extent)
+                    let newImage = UIImage(cgImage: cgimg!)
+                    return newImage
+                }
+
+            }
         }
-        return img
+        return UIImage()
     }
     
     private func grayscaleFilter(_ img: UIImage) -> UIImage {
-        let startImage = CIImage(image: img)
+        if let startImage = CIImage(image: img) {
 
-        if let filter = CIFilter(name: "CIPhotoEffectNoir") {
-            filter.setValue(startImage, forKey: kCIInputImageKey)
-            let cgImage = context.createCGImage(filter.outputImage!, from: filter.outputImage!.extent)
-            let newImage = UIImage(cgImage: cgImage!)
-            return newImage
+            if let filter = CIFilter(name: "CIPhotoEffectNoir") {
+                filter.setValue(startImage, forKey: kCIInputImageKey)
+                let cgImage = context.createCGImage(filter.outputImage!, from: filter.outputImage!.extent)
+                let newImage = UIImage(cgImage: cgImage!)
+                return newImage
+            }
         }
-        return img
+        return UIImage()
     }
     
     @IBAction func blurButton(_ sender: UIButton) {
