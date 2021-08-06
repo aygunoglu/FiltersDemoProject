d2234802dfea5704135d2934227c57c605ead2a3
aygunoglu <hasanaygunoglu@gmail.com>
2021-08-06 23:09:19 +0300

    imported BasicFilters framework into the app using cocoapods. Deleted all the filter functions that are already in the framework. app still works perfectly as expected.

--- FiltersDemoProject/ViewController.swift
+++ FiltersDemoProject/ViewController.swift
@@ -6,109 +6,25 @@
 //
 
 import UIKit
-import CoreImage
+import BasicFilters
 
 class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
 
-    var context = CIContext(options: nil)
+    let basicFilters = BasicFilters()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         imageView.image = UIImage(named: "ProfileImage")
-    }
-
-
-
-    private func blurFilter(_ img: UIImage, intensity: Double) -> UIImage? {
-        if let startImage = CIImage(image: img) {
-            
-            if let filter = CIFilter(name: "CIGaussianBlur") {
-                filter.setValue(startImage, forKey: kCIInputImageKey)
-                filter.setValue(intensity, forKey: kCIInputRadiusKey)
-                
-                if let cropFilter = CIFilter(name: "CICrop") {
-                    cropFilter.setValue(filter.outputImage, forKey: kCIInputImageKey)
-                    cropFilter.setValue(CIVector(cgRect: startImage.extent), forKey: "inputRectangle")
-                    let output = cropFilter.outputImage
-                    let cgimg = context.createCGImage(output!, from: output!.extent)
-                    let newImage = UIImage(cgImage: cgimg!)
-                    return newImage
-                }
-            }
-            
-        }
-        return UIImage()
-    }
-    
-    private func invertFilter(_ img: UIImage) -> UIImage? {
-        if let startImage = CIImage(image: img) {
-
-            if let filter = CIFilter(name: "CIColorInvert") {
-                filter.setValue(startImage, forKey: kCIInputImageKey)
-                let cgImage = context.createCGImage(filter.outputImage!, from: filter.outputImage!.extent)
-                let newImage = UIImage(cgImage: cgImage!)
-                return newImage
-            }
-        }
-        return UIImage()
-    }
-    
-    private func sepiaFilter(_ img: UIImage, intensity: Double) -> UIImage {
-        if let startImage = CIImage(image: img) {
-
-            if let filter = CIFilter(name: "CISepiaTone") {
-                filter.setValue(startImage, forKey: kCIInputImageKey)
-                filter.setValue(intensity, forKey: kCIInputIntensityKey)
-                let cgImage = context.createCGImage(filter.outputImage!, from: filter.outputImage!.extent)
-                let newImage = UIImage(cgImage: cgImage!)
-                return newImage
-            }
-        }
-        return UIImage()
-    }
-    
-    private func bloomFilter(_ img: UIImage, intensity: Double, radius: Double) -> UIImage {
-        if let startImage = CIImage(image: img) {
-
-            if let filter = CIFilter(name: "CIBloom") {
-                filter.setValue(startImage, forKey: kCIInputImageKey)
-                filter.setValue(intensity, forKey: kCIInputIntensityKey)
-                filter.setValue(radius, forKey: kCIInputRadiusKey)
-                
-                if let cropFilter = CIFilter(name: "CICrop") {
-                    cropFilter.setValue(filter.outputImage, forKey: kCIInputImageKey)
-                    cropFilter.setValue(CIVector(cgRect: startImage.extent), forKey: "inputRectangle")
-                    let output = cropFilter.outputImage
-                    let cgimg = context.createCGImage(output!, from: output!.extent)
-                    let newImage = UIImage(cgImage: cgimg!)
-                    return newImage
-                }
-
-            }
-        }
-        return UIImage()
-    }
-    
-    private func grayscaleFilter(_ img: UIImage) -> UIImage {
-        if let startImage = CIImage(image: img) {
-
-            if let filter = CIFilter(name: "CIPhotoEffectNoir") {
-                filter.setValue(startImage, forKey: kCIInputImageKey)
-                let cgImage = context.createCGImage(filter.outputImage!, from: filter.outputImage!.extent)
-                let newImage = UIImage(cgImage: cgImage!)
-                return newImage
-            }
-        }
-        return UIImage()
+        
     }
     
     @IBAction func blurButton(_ sender: UIButton) {
         print("Blur Button Pressed")
         DispatchQueue.main.async { [weak self] in
             guard let self = self else { return }
-            let processsedImg = self.blurFilter(self.imageView.image!, intensity: 5)
+            let processsedImg = self.basicFilters.blurFilter(self.imageView.image!, intensity: 5)
             DispatchQueue.main.async {
                 self.imageView.image = processsedImg
             }
@@ -119,7 +35,7 @@ class ViewController: UIViewController {
         print("Invert Button Pressed")
         DispatchQueue.main.async { [weak self] in
             guard let self = self else { return }
-            let processsedImg = self.invertFilter(self.imageView.image!)
+            let processsedImg = self.basicFilters.invertFilter(self.imageView.image!)
             DispatchQueue.main.async {
                 self.imageView.image = processsedImg
             }
@@ -130,7 +46,7 @@ class ViewController: UIViewController {
         print("Sepia Button Pressed")
         DispatchQueue.main.async { [weak self] in
             guard let self = self else { return }
-            let processsedImg = self.sepiaFilter(self.imageView.image!, intensity: 0.9)
+            let processsedImg = self.basicFilters.sepiaFilter(self.imageView.image!, intensity: 0.9)
             DispatchQueue.main.async {
                 self.imageView.image = processsedImg
             }
@@ -141,7 +57,7 @@ class ViewController: UIViewController {
         print("Bloom Button Pressed")
         DispatchQueue.main.async { [weak self] in
             guard let self = self else { return }
-            let processsedImg = self.bloomFilter(self.imageView.image!, intensity: 0.9, radius: 5)
+            let processsedImg = self.basicFilters.bloomFilter(self.imageView.image!, intensity: 0.9, radius: 5)
             DispatchQueue.main.async {
                 self.imageView.image = processsedImg
             }
@@ -152,7 +68,7 @@ class ViewController: UIViewController {
         print("Grayscale Button Pressed")
         DispatchQueue.main.async { [weak self] in
             guard let self = self else { return }
-            let processsedImg = self.grayscaleFilter(self.imageView.image!)
+            let processsedImg = self.basicFilters.grayscaleFilter(self.imageView.image!)
             DispatchQueue.main.async {
                 self.imageView.image = processsedImg
             }
---
+++ Pods/Target Support Files/BasicFilters/BasicFilters-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BasicFilters : NSObject
+@end
+@implementation PodsDummy_BasicFilters
+@end
